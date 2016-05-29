57020c29d2380cf5940416a63adffb0557e7d051
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-05-29 22:12:00 +0200

    Add copyright notice
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/*
+ * moodlights-mqtt: A RS485 <-> mqtt bridge
+ *
+ * Copyright (c) Ralf Ramsauer, 2016
+ *
+ * Authors:
+ *   Ralf Ramsauer <ralf@binary-kitchen.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 3.  See
+ * the LICENSE file in the top-level directory.
+ */
+
 #include <unistd.h>
 
 #include <iostream>
