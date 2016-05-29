b8423497bf34d97db8800aa0af543849f65c1421
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-05-29 17:22:51 +0200

    logic: make hausbus and moodlights globally available
    And add global access mutex
    
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <memory>
+#include <mutex>
+#include <regex>
+
 #include <mosquittopp.h>
 
 #include "hausbus.h"
@@ -11,6 +15,13 @@ using namespace std;
 #define MOODLIGHT_DEVICE_IDENTIFIER 0x10
 #define MY_DEVICE_IDENTIFIER 0xfe
 
+// gobal access mutex to prevent race conditions
+static std::mutex global_mutex;
+
+// allow global access to hausbus and moodlights via unique_ptrs
+static std::unique_ptr<Hausbus> hausbus = nullptr;
+static std::unique_ptr<Moodlights> moodlights = nullptr;
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         cerr << "Usage: " << argv[0] << " device_name" << endl;
@@ -20,11 +31,12 @@ int main(int argc, char **argv) {
     if (mosqpp::lib_init() != MOSQ_ERR_SUCCESS)
         throw std::runtime_error("Mosquitto initialisation failed");
 
-    Hausbus hausbus(argv[1]);
+    // Initialise Moodlights and Hausbus
+    hausbus = std::unique_ptr<Hausbus>(new Hausbus(argv[1]));
 
     // source id: 0xFE
     // destination id: 0x10, moodlights device identifier
-    Moodlights m(MY_DEVICE_IDENTIFIER, MOODLIGHT_DEVICE_IDENTIFIER);
+    moodlights = std::unique_ptr<Moodlights>(new Moodlights(MY_DEVICE_IDENTIFIER, MOODLIGHT_DEVICE_IDENTIFIER));
 
     if (mosqpp::lib_cleanup() != MOSQ_ERR_SUCCESS)
         throw std::runtime_error("Mosquitto cleanup failed");
