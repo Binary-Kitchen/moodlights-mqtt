b2cc84a361e2b85924f1b36e5527cd555f14c582
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-05-29 15:57:46 +0200

    main: Add mosquitto stubs and remove example
    Also adapt CMakeLists.txt
    
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <mosquittopp.h>
 
 #include "hausbus.h"
 #include "moodlights.h"
@@ -16,24 +17,17 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    if (mosqpp::lib_init() != MOSQ_ERR_SUCCESS)
+        throw std::runtime_error("Mosquitto initialisation failed");
+
     Hausbus hausbus(argv[1]);
 
     // source id: 0xFE
     // destination id: 0x10, moodlights device identifier
     Moodlights m(MY_DEVICE_IDENTIFIER, MOODLIGHT_DEVICE_IDENTIFIER);
 
-    // Set all lights and wait a second
-    m.set_all(Moodlights::Color{255, 0, 123});
-    hausbus << m;
-    sleep(1);
-
-    // Example:
-    // set all lamps to random level and update every 500ms
-    for (;;) {
-        m.rand_all();
-        hausbus << m;
-        usleep(500000);
-    }
+    if (mosqpp::lib_cleanup() != MOSQ_ERR_SUCCESS)
+        throw std::runtime_error("Mosquitto cleanup failed");
 
     return 0;
 }
